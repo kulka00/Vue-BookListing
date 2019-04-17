56f5834b80d0cc370306ab6ef559a5c7c4d42fa4
Kulkarni, Ankita A <patilankita63@gmail.com>
2019-04-17 16:49:01 +0200

    Completed the module

---
+++ src/components/BookItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <li>{{book.title}}:{{book.author}}</li>
+</template>
+
+<script>
+export default {
+name:BookItem,
+props:['books']
+}
+</script>
+
+<style>
+li{
+    display:block;
+    margin: 0 10px;
+}
+
+</style>
+
